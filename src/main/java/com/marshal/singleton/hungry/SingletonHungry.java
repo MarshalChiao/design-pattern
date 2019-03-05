c2e41eef2254055e64c34b87e9fdba5606300405
MarshalChiao <1105383140@qq.com>
2019-03-05 09:35:51 +0800

    initial project

---
+++ src/main/java/com/marshal/singleton/hungry/SingletonHungry.java
@@ -0,0 +1,11 @@
+package com.marshal.singleton.hungry;
+
+/**
+ * @Author: Shaoshuai Jiao
+ * @Date: Created on 2019/3/5
+ * @Time: Created at 8:59
+ * @Description:
+ */
+
+public class SingletonHungry {
+}
